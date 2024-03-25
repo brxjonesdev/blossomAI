a6a9ef5d96d39a16bf75bf7c0c3eeb26ee5c2518
Braxton Jones <58635532+Braxton-Jones@users.noreply.github.com>
2024-03-25 11:58:53 -0400

    Update route.ts

--- app/api/blsm_connect/route.ts
+++ app/api/blsm_connect/route.ts
@@ -3,8 +3,7 @@ import {NextRequest, NextResponse} from 'next/server';
 export async function GET(req: NextRequest) {
   return NextResponse.json({message: 'Hello, world!'});
 }
-
 export async function POST(req: NextRequest) {
-  console.log(req.body)
-  return NextResponse.json({message: 'Hello, world!', body: req.body});
+  const body = await req.json();
+  return NextResponse.json({message: 'Hello, world!', body: body});
 }
