852c51a84bb31fbc0ad9a417023ef23cb737aadf
Braxton <58635532+Braxton-Jones@users.noreply.github.com>
2024-03-25 16:56:10 -0400

    testing connection

--- .github/scripts/blsm.js
+++ .github/scripts/blsm.js
@@ -6,10 +6,12 @@ const issueNumber = process.env.ISSUE_NUMBER;
 const issueTitle = process.env.ISSUE_TITLE;
 const issueBody = process.env.ISSUE_BODY;
 const issueRepo = process.env.ISSUE_REPO;
+const issueAction = process.env.ISSUE_ACTION;
 const prNumber = process.env.PR_NUMBER;
 const prTitle = process.env.PR_TITLE;
 const prBody = process.env.PR_BODY;
 const prRepo = process.env.PR_REPO;
+const prAction = process.env.PR_ACTION;
 const timestamp = process.env.TIMESTAMP;
 const eventType = process.env.TYPE;
 const username = process.env.USER;
@@ -23,32 +25,34 @@ const dataFromAction = {
   username: username,
   commitDetails: {
     timestamp: timestamp,
-    username: username,
     message: commitMessage,
     repo: commitRepo,
   },
   pullRequestDetails: {
+    action: prAction,
     timestamp: timestamp,
-    username: username,
     number: prNumber,
     title: prTitle,
     body: prBody,
-    repo: prRepo,
+
   },
   issueDetails: {
+    action: issueAction,
     timestamp: timestamp,
-    username: username,
     number: issueNumber,
     title: issueTitle,
     body: issueBody,
-    repo: issueRepo,
   },
 }
 
 console.log('Data from action:', dataFromAction);
 
 function sendToBackend(data) {
-  axios.post('https://blossom-ai-rose.vercel.app/api/blsm_connect', data)
+  if (!data) {
+    console.error('No data to send');
+    return;
+  }
+  axios.post('https://blossom-ai-rose.vercel.app/api/blsm', data)
     .then(response => {
       console.log('Response from microservice:', response.data);
     })
---
+++ app/api/blsm/route.ts
@@ -0,0 +1,93 @@
+import {NextRequest, NextResponse} from 'next/server';
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export async function GET(req: NextRequest) {
+  return NextResponse.json({message: 'Hello, world!'});
+}
+
+export async function POST(req: NextRequest) {
+  const cookieStore = cookies()
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options })
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: '', ...options })
+        },
+      },
+    }
+  )
+  const body = await req.json();
+
+  if (!body || body === undefined || body === null) {
+    return NextResponse.json({ error: 'Missing body' }, { status: 400 });
+  }
+  if (!body.activityInfo.type) {
+    return NextResponse.json({ error: 'Missing body type' }, { status: 400 });
+  }
+
+  const { data: user, error } = await supabase
+    .from('users')
+    .select('*')
+    .eq('github_username', body.activityInfo.username)
+    .maybeSingle();
+  if (error) {
+    return NextResponse.json({ "Pre": error.message }, { status: 500 });
+  }
+  if (!user) {
+    const { data: newUser, error: newError } = await supabase
+      .from('users')
+      .insert([
+        {
+          githubusername: body.activityInfo.username,
+        },
+      ])
+      .single();
+    if (newError) {
+      return NextResponse.json({ error: newError.message }, { status: 500 });
+    }
+  }
+  
+
+  
+  // Check if the repo the user is sending the request for is in the database, if not, add it. If it is, proceed.
+
+
+  
+  switch (body.activityInfo.type) {
+    case 'issue':
+      console.log('Received issue body:', body.issueDetails);
+      // Process the issue body here
+
+
+
+
+    return NextResponse.json({ message: 'Received issue body & was succesfully sent to BLSM Database <3 <3 <3'});
+    case 'pull_request':
+      console.log('Received pull request body:', body.pullRequestDetails);
+      // Process the pull request body here
+
+
+
+      return NextResponse.json({ message: 'Received pull request body & was succesfully sent to BLSM Database <3 <3 <3'});
+    case 'push':
+      console.log('Received commit body:', body.commitDetails);
+      // Process the commit body here
+
+
+
+      return NextResponse.json({ message: 'Received commit body & was succesfully sent to BLSM Database <3 <3 <3', data: body.commitDetails});
+    default:
+      console.log('Invalid body type received:', body.type);
+  }
+  return NextResponse.json({message: 'Hello, world!', body: body}, { status: 200 });
+}
