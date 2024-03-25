1751349f770b405ab6765f547dd02bb45a56db0a
Braxton <58635532+Braxton-Jones@users.noreply.github.com>
2024-03-25 13:35:03 -0400

    route.ts

--- app/api/blsm_connect/route.ts
+++ app/api/blsm_connect/route.ts
@@ -4,6 +4,7 @@ import { createClient } from '@/utils/supabase/server';
 export async function GET(req: NextRequest) {
   return NextResponse.json({message: 'Hello, world!'});
 }
+
 export async function POST(req: NextRequest) {
   const supabase = createClient();
   const body = await req.json();
