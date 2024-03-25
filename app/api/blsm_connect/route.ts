c7af654dad19692ddf2a29a31a224aefd526d275
Braxton <58635532+Braxton-Jones@users.noreply.github.com>
2024-03-25 12:06:36 -0400

    update rooute

--- app/api/blsm_connect/route.ts
+++ app/api/blsm_connect/route.ts
@@ -5,5 +5,30 @@ export async function GET(req: NextRequest) {
 }
 
 export async function POST(req: NextRequest) {
-  return NextResponse.json({message: req});
+  const body = await req.json();
+  if (!body) {
+    return NextResponse.json({ error: 'Missing body' }, { status: 400 });
+  }
+
+  if (!body.type) {
+    return NextResponse.json({ error: 'Missing body type' }, { status: 400 });
+  }
+
+  switch (body.type) {
+    case 'issue':
+      console.log('Received issue body:', body.issueDetails);
+      // Process the issue body here
+    return NextResponse.json({ message: 'Received issue body' });
+    case 'pull_request':
+      console.log('Received pull request body:', body.pullRequestDetails);
+      // Process the pull request body here
+      return NextResponse.json({ message: 'Received pull request body' });
+    case 'push':
+      console.log('Received commit body:', body.commitDetails);
+    //   send body to the bodybase
+      return NextResponse.json({ message: 'Received commit body' });
+    default:
+      console.log('Invalid body type received:', body.type);
+  }
+  return NextResponse.json({message: 'Hello, world!', body: body}, { status: 200 });
 }
