e84d8568688262d29fe942ff5344f5b0fb2f06ac
Braxton <Braxtonjonesttsl@gmail.com>
2024-03-27 12:41:19 -0400

    Update home and user-info components

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import Notifications from '@/components/user-info';
-import Projects from '@/components/projects';
-
-
-export default function Dashboard() {
-  return <Projects />;
+export default function BLSMHome() {
+  return (
+    <section>
+      <div  className='flex flex-col gap-4'>
+      <h1 className='font-black text-2xl text-center'>
+        {`Connect your project to `} <span className='text-blsm_accent underline-offset-2 hover:underline-offset-4 underline transition-all ease-in-out'>BlossomAI</span> {`and start sharing your project's updates.`}
+      </h1>
+      <p className='text-center font-cabin'>
+        {`Here's how to setup your project with BlossomAI.`}
+        {/* 
+          Actual steps to connect to BlossomAI:
+          1. Sign in to your BlossomAI account.
+          2. Link your GitHub account.
+          3. Users just need to tell us what branches to monitor.
+          4. The script is made available to users to add to their GitHub repository.
+        
+        */}
+      </p>
+      </div>
+    </section>
+  );
 }
---
+++ script-teaser.rs
@@ -0,0 +1,12 @@
+// okay i have never used rust
+// but i think i can do this
+
+// this is what the script should do
+
+// the inputs are "branch_name", "repo_name" and "github_username"
+
+{/*
+    1. attempts to add the repo to the BLSM db, if it already exists, it will return an error.
+    2. it will then create the setup files for using BLSM
+    
+*/}
